767092bd527ea3aebeec9d181ab1a9194f471b52
ajraj27 <rajanuj.iiita@gmail.com>
2019-03-08 00:14:10 +0530

    Completed basic backend

--- server.js
+++ server.js
@@ -7,64 +7,52 @@ const app=express();
 app.use(bodyParser.json());
 
 app.post('/getIssuesInfo',async(req,res) => {
-  //console.log(req.body);
+  const curTime=new Date().getTime();
+  const oneDayTime=1*24*60*60*1000;
+  const last1DayTimeISO=new Date(curTime-oneDayTime).toISOString();
+  const last7DaysTimeISO=new Date(curTime-7*oneDayTime).toISOString();
+
   const url=req.body.url;
   const arrUrl=url.split("/");
+
   const apiUrl=`https://api.github.com/repos/${arrUrl[3]}/${arrUrl[4]}`;
-   console.log(apiUrl);
-  let openIssuesCount=await axios.get(apiUrl);
-  openIssuesCount=openIssuesCount.data.open_issues_count;
-  res.send({openIssuesCount})
+  const openIssuesCount=(await axios.get(apiUrl)).data.open_issues_count;
+
+  let flag=true,ctr=1,last1DayCount=0;
+
+  while(flag){
+    const res=await axios.get(apiUrl+`/issues?page=${ctr}&per_page=100&since=${last1DayTimeISO}`);
+    const len=res.data.length;
+
+    if(!len)
+      break;
+
+    last1DayCount+=len;
+    ctr++;
+  }
+
+  ctr=1,last7DaysCount=0;
+
+  while(flag){
+    const res=await axios.get(apiUrl+`/issues?page=${ctr}&per_page=100&since=${last7DaysTimeISO}`);
+    console.log(res.data);
+    const len=res.data.length;
+
+    if(!len)
+      break;
+
+    last7DaysCount+=len;
+    ctr++;
+  }
+
+  res.send({
+    "TotalOpenIssuesCount":openIssuesCount,
+    "LastOneDayOpenedIssuesCount":last1DayCount,
+    "LastSevenDaysButMoreThanOneDayCount":last7DaysCount-last1DayCount,
+    "BeforeSevenDaysIssuesCount":openIssuesCount-last7DaysCount
+  });
 });
 
 app.listen(3000,() => {
   console.log("Server started!");
 });
-
-
-// // fetch('https://api.github.com/repos/octocat/Hello-World').then((res) => {
-// //   return res.json();
-// // }).then((res) => {
-// //   console.log(`Open Issues Count: ${res.open_issues_count}`);
-// // }).catch((err) => console.log(err));
-//
-// async function getTotalOpenIssuesCount(){
-//   const res=await axios.get('https://api.github.com/repos/octocat/Hello-World');
-//   return res;
-//
-// }
-//
-// const count= await getTotalOpenIssuesCount();
-// console.log(count);
-//
-//
-//
-// const curTime=new Date().getTime();
-// const oneDayTime=1*24*60*60*1000;
-// const last1DayTimeISO=new Date(curTime-oneDayTime).toISOString();
-// const last7DaysTimeISO=new Date(curTime-7*oneDayTime).toISOString();
-//
-//  console.log(last1DayTimeISO);
-//  console.log(last7DaysTimeISO);
-//
-//  var flag=true,ctr=1,sum=0;
-//
-//  // while(flag){
-//  //   console.log(flag,ctr,sum);
-//  //   fetch(`https://api.github.com/repos/octocat/Hello-World/issues?page=${ctr}`)
-//  //   .then((res) => res.json())
-//  //   .then((res) => {
-//  //     console.log("Hello");
-//  //     if(res.length===0){
-//  //       flag=false;
-//  //     }
-//  //     sum+=res.length;
-//  //   })
-//  //   .catch((err) => console.log(err));
-//  //   ctr++;
-//  // }
-//
-// // fetch('https://api.github.com/repos/octocat/Hello-World/issues')
-// // .then((res) => res.json())
-// // .then((res) => console.log(res))
-// // .catch((err) => console.log(err));
