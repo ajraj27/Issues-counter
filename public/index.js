d4f8bfa42bd347ec0d1fed5bd3fdf5aa546682f8
ajraj27 <rajanuj.iiita@gmail.com>
2019-03-11 17:54:44 +0530

    completed both

--- public/index.js
+++ public/index.js
@@ -5,12 +5,17 @@ const dataValue1=document.querySelector('.data-1');
 const dataValue2=document.querySelector('.data-2');
 const dataValue3=document.querySelector('.data-3');
 const dataValue4=document.querySelector('.data-4');
+const backdrop=document.querySelector('.Backdrop');
 
 submitBtn.addEventListener('click',async (e) => {
     e.preventDefault();
     const url=document.querySelector('input[type=text]').value;
 
     if(url){
+        console.log(backdrop.style);
+        backdrop.style.display="block";
+        console.log("hey");
+        
         const res=await axios({
             method:'post',
             url:'/getIssuesInfo',
@@ -21,10 +26,12 @@ submitBtn.addEventListener('click',async (e) => {
         dataValue2.innerHTML=res.data["LastOneDayOpenedIssuesCount"];
         dataValue3.innerHTML=res.data[ "LastSevenDaysButMoreThanOneDayCount"];
         dataValue4.innerHTML=res.data["BeforeSevenDaysIssuesCount"];
+
+        backdrop.style.display="none";
     }
     
     else{
-        console.log("Enter something");
+        alert("Enter url exactly of the form : https://github.com/{username}/{repo_name}");
     }
 });
 
